52aac4f5516008f962eeef7c53880153a3ba6eba
smallcat9603 <huyao0107@gmail.com>
2024-07-18 21:20:00 +0900

    minor

--- waitio/test/pingpong/pingpong.o.sh
+++ waitio/test/pingpong/pingpong.o.sh
@@ -2,11 +2,11 @@
 
 #------ pjsub option --------#
 #PJM -L rscgrp=regular-o
-#PJM -L node=2x4:torus
-#PJM --mpi proc=16
+#PJM -L node=2
+#PJM --mpi proc=2
 #PJM -L elapse=1:00:00
 #PJM -g gz00
 #PJM -j
 
 #------- Program execution -------#
-mpiexec /work/gz00/z30130/mpi/app/bin/lu 1024
+mpiexec /work/gz00/z30130/mpi/waitio/test/pingpong/pingpong
